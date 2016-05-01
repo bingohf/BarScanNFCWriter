f71f3c5a315f69ddc095f8ee5bfb06f36f990e86
bingohf <togb@qq.com>
2016-05-01 13:34:17 +0800

    add moudle

---
+++ bundlechecking/src/main/java/com/ledway/bundlechecking/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ledway.bundlechecking;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by togb on 2016/5/1.
+ */
+public class MainActivity extends AppCompatActivity {
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
