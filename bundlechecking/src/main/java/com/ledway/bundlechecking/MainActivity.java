b36c96d93ba9289d40877375fb2fc0e63c4fe534
bingohf <togb@qq.com>
2016-05-01 15:06:00 +0800

    framework done

--- bundlechecking/src/main/java/com/ledway/bundlechecking/MainActivity.java
+++ bundlechecking/src/main/java/com/ledway/bundlechecking/MainActivity.java
@@ -1,12 +1,15 @@
 package com.ledway.bundlechecking;
 
+import android.app.AlertDialog;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.nfc.tech.MifareClassic;
 import android.nfc.tech.NfcA;
 import android.os.Bundle;
@@ -14,46 +17,89 @@ import android.os.Vibrator;
 import android.serialport.api.SerialPort;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import com.zkc.Service.CaptureService;
+import com.zkc.beep.ServiceBeepManager;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 import java.util.regex.Pattern;
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action0;
+import rx.subscriptions.CompositeSubscription;
+import rx.subscriptions.Subscriptions;
 
 /**
  * Created by togb on 2016/5/1.
  */
 public class MainActivity extends AppCompatActivity {
-  @BindView(R.id.txt_barcode) EditText mEdtBarCode;
-  private Vibrator vibrator;
-  private NfcAdapter nfcAdapter;
-  private BroadcastReceiver scanBroadcastReceiver = new BroadcastReceiver() {
+  private static final String TAG = "MainActivity";
+  private byte keyA[] = { (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff,
+      (byte) 0xff, (byte) 0xff};
 
-    @Override public void onReceive(Context context, Intent intent) {
+  private NfcAdapter nfcAdapter;
+  private PendingIntent pendingIntent;
+  private IntentFilter[] mFilters;
+  private String[][] mTechLists;
+  private Intent intents;
+  EditText mEdtBarCode;
+  private ServiceBeepManager beepManager;
+  private CompositeSubscription subscriptions = new CompositeSubscription();
+  private BroadcastReceiver scanBroadcastReceiver = new BroadcastReceiver(){
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
       String text = intent.getExtras().getString("code");
-      if (text.length() < 10) {
+      if (text.length()<10){
         Toast.makeText(MainActivity.this, R.string.invalid_barcode, Toast.LENGTH_LONG).show();
       }
+      Log.i(TAG, "MyBroadcastReceiver code:" + text);
       Pattern pattern = Pattern.compile("[^0-9a-zA-Z_ ]");
-      if (!pattern.matcher(text).matches()) {
+      if(!pattern.matcher(text).matches()) {
         mEdtBarCode.setText(text);
-      } else {
+      }else{
         vibrator.vibrate(1000);
       }
     }
   };
-  private String[][] mTechLists;
-  private IntentFilter[] mFilters;
-  private PendingIntent pendingIntent;
 
-  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+
+  private View mBtnClear;
+  private ListView mListRecord;
+  private Vibrator vibrator;
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    ButterKnife.bind(this);
-    vibrator = (Vibrator) getSystemService(Service.VIBRATOR_SERVICE);
+    mEdtBarCode = (EditText) findViewById(R.id.txt_barcode);
+    findViewById(R.id.my_layout).requestFocus();
+
+    //mListRecord.scrollTo(0,100000);
     nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+    vibrator=(Vibrator)getSystemService(Service.VIBRATOR_SERVICE);
+
 
     pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,
         getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
@@ -67,11 +113,58 @@ public class MainActivity extends AppCompatActivity {
     Intent newIntent = new Intent(MainActivity.this, CaptureService.class);
     newIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP );
     startService(newIntent);
+    subscriptions.add(Observable.create(new Observable.OnSubscribe<Object>() {
+      @Override
+      public void call(Subscriber<? super Object> subscriber) {
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.zkc.scancode");
+        MainActivity.this.registerReceiver(scanBroadcastReceiver, intentFilter);
+        subscriber.add(Subscriptions.create(new Action0() {
+          @Override
+          public void call() {
+            MainActivity.this.unregisterReceiver(scanBroadcastReceiver);
+          }
+        }));
+      }
+    }).subscribe());
+    findViewById(R.id.btn_scan).setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        SerialPort.CleanBuffer();
+        CaptureService.scanGpio.openScan();
+      }
+    });
+
+    mEdtBarCode.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+      @Override public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        if (actionId == EditorInfo.IME_ACTION_SEARCH){
+          return  true;
+        }
+        return  false;
+      }
+    });
   }
 
-  @OnClick(R.id.btn_scan)
-  void btnScanClock(){
-    SerialPort.CleanBuffer();
-    CaptureService.scanGpio.openScan();
+
+  private void exitActivity() {
+    new AlertDialog.Builder(this)
+        .setIcon(android.R.drawable.ic_dialog_alert)
+        .setTitle(R.string.exit)
+        .setMessage(R.string.exit_confirm)
+        .setPositiveButton(R.string.yes,
+            new DialogInterface.OnClickListener() {
+              @Override
+              public void onClick(DialogInterface dialog,
+                  int which) {
+
+                CaptureService.scanGpio.closeScan();
+                CaptureService.scanGpio.closePower();
+                finish();
+              }
+            }).setNegativeButton(R.string.no, null).show();
   }
+
+
+
+
+
 }
