42d9982169fa2992e6bcd93d74adc05d81820041
bingohf <93707037@qq.com>
2017-10-06 18:13:18 +0800

    add product total user

--- btprinter/src/main/java/com/ledway/btprinter/network/DBService.java
+++ btprinter/src/main/java/com/ledway/btprinter/network/DBService.java
@@ -8,9 +8,11 @@ import com.ledway.btprinter.network.model.SpReturn;
 import com.ledway.btprinter.network.model.Sp_UpSampleDetail_Request;
 import com.ledway.btprinter.network.model.Sp_UpSampleDetail_Return;
 import com.ledway.btprinter.network.model.Sp_UpSample_v3_Request;
+import com.ledway.btprinter.network.model.TotalUserReturn;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 import rx.Observable;
 
@@ -31,4 +33,6 @@ public interface DBService {
 
   @POST("Sp/sp_UpSampleDetail") Observable<RestSpResponse<Sp_UpSampleDetail_Return>> sp_UpSampleDetail(@Body
       Sp_UpSampleDetail_Request request);
+
+  @GET("sql/{sql}") Observable<RestDataSetResponse<TotalUserReturn>> queryTotalUser(@Path("sql") String sql);
 }
---
+++ btprinter/src/main/java/com/ledway/btprinter/network/model/TotalUserReturn.java
@@ -0,0 +1,9 @@
+package com.ledway.btprinter.network.model;
+
+/**
+ * Created by togb on 2017/10/6.
+ */
+
+public class TotalUserReturn {
+  public int total_users;
+}
