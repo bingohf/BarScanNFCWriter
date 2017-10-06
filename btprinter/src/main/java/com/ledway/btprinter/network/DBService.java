d5ab42478abe5d11052cffb4b32ec1344bc1b2f6
bingohf <93707037@qq.com>
2017-10-06 16:47:47 +0800

    add product Get

--- btprinter/src/main/java/com/ledway/btprinter/network/DBService.java
+++ btprinter/src/main/java/com/ledway/btprinter/network/DBService.java
@@ -1,9 +1,9 @@
 package com.ledway.btprinter.network;
 
 import com.ledway.btprinter.network.model.ProductAppGetReturn;
+import com.ledway.btprinter.network.model.ProductReturn;
 import com.ledway.btprinter.network.model.RestDataSetResponse;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
 import rx.Observable;
 
@@ -12,6 +12,10 @@ import rx.Observable;
  */
 
 public interface DBService {
-  @GET("dataset/PRODUCTAPPGET")  Observable<RestDataSetResponse<ProductAppGetReturn>> getProductAppGet( @Query("getProductAppGet") String query,
-      @Query("orderBy") String orderBy);
+  @GET("dataset/PRODUCTAPPGET")
+  Observable<RestDataSetResponse<ProductAppGetReturn>> getProductAppGet(
+      @Query("query") String query, @Query("orderBy") String orderBy);
+
+  @GET("dataset/product") Observable<RestDataSetResponse<ProductReturn>> getProduct(
+      @Query("query") String query, @Query("orderBy") String orderBy);
 }
