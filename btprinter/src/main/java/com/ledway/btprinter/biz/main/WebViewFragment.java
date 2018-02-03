c20596e82e3f1c03fdf0525b7ee40eb2a2333305
bingohf <93707037@qq.com>
2018-02-03 09:02:52 +0800

    webview  open pdf with default browser

--- btprinter/src/main/java/com/ledway/btprinter/biz/main/WebViewFragment.java
+++ btprinter/src/main/java/com/ledway/btprinter/biz/main/WebViewFragment.java
@@ -1,5 +1,7 @@
 package com.ledway.btprinter.biz.main;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -62,8 +64,13 @@ public class WebViewFragment extends Fragment implements OnKeyPress {
 
     mWebView.setWebViewClient(new WebViewClient() {
       @Override public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        // view.loadUrl(url);
-        return false;
+        if (url != null && url.endsWith("pdf")) {
+          view.getContext().startActivity(
+              new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+          return true;
+        } else {
+          return false;
+        }
       }
 
       @Override public void onPageFinished(WebView view, String url) {
