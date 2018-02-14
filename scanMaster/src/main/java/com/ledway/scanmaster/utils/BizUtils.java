1039cdf33795983516b00435f31cb98708c5ae67
bingohf <93707037@qq.com>
2018-02-14 10:21:25 +0800

    add bizUtils

---
+++ scanMaster/src/main/java/com/ledway/scanmaster/utils/BizUtils.java
@@ -0,0 +1,34 @@
+package com.ledway.scanmaster.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.provider.Settings;
+import android.text.TextUtils;
+import com.ledway.scanmaster.AppConstants;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by togb on 2018/2/14.
+ */
+
+public class BizUtils {
+  public static String getMyTaxNo(Context context){
+
+    final String DATA_FORMAT ="yyyyMMdd'T'HHmmss.S";
+    final String ID_FORMAT ="%s-%s-LEDWAY-%s";
+    SharedPreferences sp =
+        context.getSharedPreferences(AppConstants.SP_NAME, Context.MODE_PRIVATE);
+    String taxNo = sp.getString("MyTaxNo", "");
+    if(!TextUtils.isEmpty(taxNo)) {
+      return taxNo;
+    }else {
+      String mDeviceId =
+          Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+      String mDeviceName = Build.MODEL;
+      SimpleDateFormat mSimpleDateFormater = new SimpleDateFormat(DATA_FORMAT);
+      return String.format(ID_FORMAT,  mDeviceId,mDeviceName, mSimpleDateFormater.format(new Date()));
+    }
+  }
+}
